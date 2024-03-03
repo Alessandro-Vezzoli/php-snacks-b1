6c010810485b84b42eb6ba6b273ff68fc8af3fab
VEZZO\alexv <alessandro.vezzoli@vp360.eu>
2024-03-03 01:21:50 +0100

    finish snack 5

---
+++ index_snack5.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
+Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+Ogni punto un nuovo paragrafo.
+*/
+
+$paragrafo = "Questo è un paragrafo abbastanza lungo. Contiene diverse frasi. Sarà suddiviso in tanti paragrafi. Ogni punto indicherà un nuovo paragrafo.";
+
+$paragrafi = explode('.', $paragrafo);
+
+
+foreach ($paragrafi as $p) {
+    echo "<p>$p</p>";
+}
+
+?>
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="" async defer></script>
+    </body>
+</html>
